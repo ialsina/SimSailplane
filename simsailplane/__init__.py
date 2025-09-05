8a8a7426a75908ea57984a8cba8f62f4e9edfcab
Ivan Alsina <1050-ialsina@users.noreply.bsc.es>
2025-09-05 08:59:32 +0200

    Add __init__ file

---
+++ simsailplane/__init__.py
@@ -0,0 +1,11 @@
+from .action import ActionController, ActionControllerLazy
+from .simulation import Sailplane6DOF, PointMassGlide
+from .plotter import SolViewer
+
+__all__ = [
+    "ActionController",
+    "ActionControllerLazy",
+    "Sailplane6DOF",
+    "PointMassGlide",
+    "SolViewer",
+]
